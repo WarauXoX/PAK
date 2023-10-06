5482957dd8d072777c3bc2ed45a285cc30cd424f
WarauXoX <slava.morlang@gmail.com>
2023-10-06 07:35:24 +0500

    Made the pop-up work

---
+++ assets/JS/script.js
@@ -0,0 +1,18 @@
+let popupBg = document.querySelector('.pop-up-bg');
+let popup = document.querySelector('.pop-up'); // Само окно
+let openPopupButtons = document.querySelectorAll('#start'); // Кнопки для показа окна
+let closePopupButton = document.querySelector('.close-pop-up'); // Кнопка для скрытия окна
+
+openPopupButtons.forEach((button) => { // Перебираем все кнопки
+    button.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
+        e.preventDefault(); // Предотвращаем дефолтное поведение браузера
+        popupBg.classList.add('active'); // Добавляем класс 'active' для фона
+        popup.classList.add('active'); // И для самого окна
+    })
+});
+document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
+    if(e.target === popupBg) { // Если цель клика - фот, то:
+        popupBg.classList.remove('active'); // Убираем активный класс с фона
+        popup.classList.remove('active'); // И с окна
+    }
+});
