e6d541ffd8cacd04b6be56b81165349e30553a00
WarauXoX <slava.morlang@gmail.com>
2023-10-23 15:40:38 +0500

    Fuck JS

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -16,3 +16,59 @@ document.addEventListener('click', (e) => {
         popup.classList.remove('active'); 
     }
 });
+let n = document.createElement('div');
+n.innerHTML = `<h1>hee</h1>`
+
+
+
+document.querySelector('.left-block').addEventListener("click", (e) => {
+    if(e.target.class != "changOpen"){
+        e.target.class = "changOpen";
+        return buts = new chenging(e.target.id, e.target)
+    }
+
+    else{ 
+        if(!buts.isHide){
+            console.log(buts.isHide)
+            buts.close() 
+        }
+        else{
+            buts.open()
+            console.log(buts.isHide)
+        }
+    }
+
+
+});
+
+class blocks{
+    block
+    
+} 
+class chenging extends(blocks){
+    _elem = document.createElement("div").innerHTML = `        <div>
+    <button>a</button>
+    <button>b</button>
+    <button>c</button>
+    <button>d</button>
+</div>`
+    
+    constructor(id, idParent){
+        super()
+        this.id = id;
+        this.idParent = idParent;
+        this.hide = false;
+    }
+
+    open(){
+        this.isHide = false;
+
+        console.log(this.isHide)
+        this.idParent.removeChild(this._elem)
+    }
+    close(){
+        this.isHide = true;
+        console.log(this.isHide)
+        this.idParent.appendChild(this._elem);
+    }
+}
